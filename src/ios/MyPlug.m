507001daf727f846ffdb5d2eb93d24a3ea42c9ea
sandeep <sandeep.rawat@evontech.com>
2018-11-22 16:24:43 +0530

    minor change

--- src/ios/MyPlug.m
+++ src/ios/MyPlug.m
@@ -29,7 +29,7 @@
 - (void)add:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"iOS- You called add function"];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"iOS- You called add function"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
